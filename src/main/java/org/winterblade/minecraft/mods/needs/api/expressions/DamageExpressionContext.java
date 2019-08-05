aa7c157e6e2b0bb9e4e3ebc272cc1c1f7952fe2a
Matt <legendblade@users.noreply.github.com>
2019-08-05 12:17:45 -0700

    Forgot the deserializer

--- src/main/java/org/winterblade/minecraft/mods/needs/api/expressions/DamageExpressionContext.java
+++ src/main/java/org/winterblade/minecraft/mods/needs/api/expressions/DamageExpressionContext.java
@@ -1,7 +1,10 @@
 package org.winterblade.minecraft.mods.needs.api.expressions;
 
+import com.google.gson.annotations.JsonAdapter;
+
 import java.util.List;
 
+@JsonAdapter(ExpressionContext.Deserializer.class)
 public class DamageExpressionContext extends NeedExpressionContext {
     public static final String AMOUNT = "amount";
 
